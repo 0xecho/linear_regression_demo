96e8c05a8a21c4d5a7db3b80644eaa5abedf11cf
0xecho <0xecho@proto-code.com>
2022-01-22 22:45:48 +0300

    Adds FA

--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -3,5 +3,12 @@ import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrashAlt, faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faTrashAlt, faPlus)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(BootstrapVue)
